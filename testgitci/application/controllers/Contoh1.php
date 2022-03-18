54dc35aca4a7db7e17e19495437aaa05beb24e1a
fachry <test@gmail.com>
2022-03-18 21:59:29 +0700

    menambahkan folder CI Contoh 1

---
+++ testgitci/application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+	class Contoh1 extends CI_controller
+{
+	public function index()
+	{
+		echo "<h1> Perkenalkan </h1>";
+		echo "Nama Saya Axel
+			  Saya tinggal didaerah sukahati
+			  nganime adalah hobi saya";
+	}
+}
+?>
