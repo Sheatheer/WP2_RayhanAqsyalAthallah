2de4b09516752998eca05e16086e3ca5343b1599
Axel <--replace-all>
2022-03-26 15:12:12 +0700

    Upload Folder Tugas_2

---
+++ Tugas_2/application/views/view-latihan1.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title> Latihan 2 </title>
+</head>
+<body>
+Ini hasil Code <br>
+Nilai 1 = <?= $nilai1 ?><br>
+Nilai 2 = <?= $nilai2 ?><br>
+Hasil penjumlahannya yaitu <?= $nilai1 . " + " . $nilai2 . " = " .$hasil; ?>
+</body>
+</html>
