7caaace76de57b0e4a9394a2c8ea44f2ae53e9f7
Axel <--replace-all>
2022-03-26 14:17:58 +0700

    test

---
+++ Code_Ig/application/views/view-latihan1.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title> Latihan 1 </title>
+</head>
+<body>
+<h1>Halo</h1><br>
+Nilai 1 = <?= $nilai1; ?> <br>
+Nilai 2 = <?= $nilai2; ?> <br>
+Hasilnya <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
